6f85876de5ada9ca7c22dcdfecc31465f538cbd5
Tejas S <128490171+tejas004s@users.noreply.github.com>
2024-07-16 12:35:27 +0530

    Update titanic.py

--- Random forest model/Linear regression model/Titanic/titanic.py
+++ Random forest model/Linear regression model/Titanic/titanic.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 titanic_data = pd.read_csv('train.csv')
@@ -17,7 +17,7 @@ y = titanic_data['Survived']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = LogisticRegression()
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
@@ -26,10 +26,7 @@ accuracy = accuracy_score(y_test, y_pred)
 conf_matrix = confusion_matrix(y_test, y_pred)
 class_report = classification_report(y_test, y_pred)
 
-print(f'Accuracy: {accuracy}')
-print(f'Confusion Matrix:\n{conf_matrix}')
-print(f'Classification Report:\n{class_report}')
-
+# Prepare the submission file
 gender_submission = X_test.copy()
 gender_submission['Survived'] = y_pred
 gender_submission['Sex'] = titanic_data.loc[X_test.index, 'Sex']
